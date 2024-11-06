1167e08123681444ce78c4428f646c37e7d21cc0
nielassis <nielvitorba@gmail.com>
2024-11-06 20:04:44 -0300

    feat: add transaction type badge

---
+++ app/transactions/page.tsx
@@ -0,0 +1,22 @@
+import { ArrowDownUpIcon } from "lucide-react";
+import { Button } from "../_components/ui/button";
+import { db } from "../_lib/prisma";
+import { DataTable } from "../_components/ui/data-table";
+import { TransactionColumns } from "./_columns";
+
+const TransactionsPage = async () => {
+  const transactions = await db.transaction.findMany({});
+  return (
+    <div className="space-y-6 p-6">
+      <div className="flex w-full items-center justify-between">
+        <h1 className="text-2xl font-bold">Transações</h1>
+        <Button className="rounded-full">
+          Adicionar Transação <ArrowDownUpIcon />
+        </Button>
+      </div>
+      <DataTable data={transactions} columns={TransactionColumns} />
+    </div>
+  );
+};
+
+export default TransactionsPage;
