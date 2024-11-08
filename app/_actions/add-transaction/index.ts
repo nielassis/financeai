777281541fb055572991a3aaff178548d0b016c5
nielassis <nielvitorba@gmail.com>
2024-11-08 16:13:36 -0300

    fix: upsert transaction function

--- app/_actions/add-transaction/index.ts
+++ app/_actions/add-transaction/index.ts
@@ -28,19 +28,19 @@ export const upsertTransaction = async (params: UpsertTransactionParams) => {
     throw new Error("Unauthorized");
   }
 
-  if (params.id) {
-    await db.transaction.upsert({
-      where: {
-        id: params.id,
-      },
-      update: { ...params, userId },
-      create: { ...params, userId },
-    });
-  } else {
-    await db.transaction.create({
-      data: { ...params, userId },
-    });
-  }
+  await db.transaction.upsert({
+    update: {
+      ...params,
+      userId,
+    },
+    create: {
+      ...params,
+      userId,
+    },
+    where: {
+      id: params.id ?? "",
+    },
+  });
 
   revalidatePath("/transactions");
 };
