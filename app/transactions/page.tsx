132d6ffed5ab7f61edace26601c6b9d81eb6b8c2
nielassis <nielvitorba@gmail.com>
2024-11-10 11:04:43 -0300

    feat: ensure pages are scolled correctly

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -5,6 +5,7 @@ import TransactionButton from "../_components/add-transaction-button";
 import NavBar from "../_components/navBar";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+import { ScrollArea } from "../_components/ui/scroll-area";
 
 const TransactionsPage = async () => {
   const { userId } = await auth();
@@ -19,12 +20,14 @@ const TransactionsPage = async () => {
   return (
     <>
       <NavBar />
-      <div className="space-y-6 p-6">
+      <div className="space-y-6 overflow-hidden p-6">
         <div className="flex w-full items-center justify-between">
           <h1 className="text-2xl font-bold">Transações</h1>
           <TransactionButton />
         </div>
-        <DataTable data={transactions} columns={TransactionColumns} />
+        <ScrollArea>
+          <DataTable data={transactions} columns={TransactionColumns} />
+        </ScrollArea>
       </div>
     </>
   );
