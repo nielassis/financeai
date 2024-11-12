8b1092e9502981cd20715f73d7b3095a23e6b44b
nielassis <nielvitorba@gmail.com>
2024-11-12 20:52:45 -0300

    feat: add reports page

---
+++ app/reports/_components/page-state.tsx
@@ -0,0 +1,56 @@
+import { Button } from "@/app/_components/ui/button";
+import { auth, clerkClient } from "@clerk/nextjs/server";
+import { EyeOff, TriangleAlert } from "lucide-react";
+import Link from "next/link";
+
+const PageState = async () => {
+  const { userId } = await auth();
+  if (!userId) {
+    throw new Error("Usuário não identificado");
+  }
+  const user = await clerkClient().users.getUser(userId);
+  const HasProPlan = user?.publicMetadata.subscriptionPlan == "pro";
+
+  return (
+    <div className="flex justify-center">
+      {!HasProPlan ? (
+        <div className="flex flex-col items-center justify-center gap-3">
+          <EyeOff className="text-primary" size={48} />
+          <p className="pt-6 text-muted-foreground">
+            Para usar essa ferramenta, você precisa ter uma assinatura Pro.
+          </p>
+          <div className="flex w-full items-center justify-center gap-3 pt-6">
+            <Link href="/subscription">
+              <Button className="font-bold">Fazer Upgrade!</Button>
+            </Link>
+            <Link href="/">
+              <Button variant="outline" className="font-bold">
+                Voltar ao dashboard
+              </Button>
+            </Link>
+          </div>
+        </div>
+      ) : (
+        <div className="flex flex-col items-center justify-center gap-3">
+          <TriangleAlert className="text-primary" size={48} />
+          <p className="pt-6 text-muted-foreground">
+            Função Em Desenvolvimento
+          </p>
+          <p className="text-muted-foreground">
+            Nos desulpe, essa função está em desenvolvimento, prometemos
+            corrigir o mais rapido possível.
+          </p>
+          <div className="flex w-full items-center justify-center gap-3 pt-6">
+            <Link href="/dashboard">
+              <Button className="font-bold" variant="ghost">
+                Ir para o Dashboard
+              </Button>
+            </Link>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default PageState;
---
+++ app/reports/page.tsx
@@ -0,0 +1,16 @@
+import NavBar from "../_components/navBar";
+import PageState from "./_components/page-state";
+
+const ReportsṔage = () => {
+  return (
+    <>
+      <NavBar />
+      <div className="space-y-6 overflow-hidden p-6">
+        <h1 className="text-2xl font-bold">Relatórios</h1>
+        <PageState />
+      </div>
+    </>
+  );
+};
+
+export default ReportsṔage;
